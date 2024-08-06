fe814e1d3b0f01eefaacca83b2c6fbd7a0abaf20
Tarun Chandrasekaran <tchan0524@gmail.com>
2024-08-06 15:47:05 -0500

    react app api rul api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
    //const API_URL = 'https://onemillionhaas-006c54c385ff.herokuapp.com/api';
-   const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+   //const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+   const API_URL = process.env.REACT_APP_API_URL || '   https://onemillionhaas-006c54c385ff.herokuapp.com/';
+
 
    const api = axios.create({
      baseURL: API_URL,
